0dce4189bcc79bf613303303ca42e07e5298a3c5
Lumaviug <152444105+Lumaviug@users.noreply.github.com>
2024-11-05 20:16:58 +0100

    carusel 2.0
    todavía no está bien del todo

--- js/script.js
+++ js/script.js
@@ -78,3 +78,27 @@ document.addEventListener("scroll", function() {
         navbar.classList.remove("scrolled");
     }
 });
+
+// ARTISTAS 
+
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
